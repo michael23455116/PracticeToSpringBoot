d6a4b3492b1ed91ad5e31676de71da59bf3dc76f
michael23455116 <michael23455116@gmail.com>
2023-10-24 14:16:45 +0800

    商品功能：實作查詢商品列表

--- src/main/java/com/example/ecommerce/service/ProductService.java
+++ src/main/java/com/example/ecommerce/service/ProductService.java
@@ -3,7 +3,10 @@ package com.example.ecommerce.service;
 import com.example.ecommerce.dto.ProductRequest;
 import com.example.ecommerce.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId,ProductRequest productRequest);
