0ecd00562a1b28fc52cb9bb74528f456b421ddaa
michael23455116 <michael23455116@gmail.com>
2023-11-03 20:13:20 +0800

    帳號功能－檢查註冊email及隱藏密碼

--- src/main/java/com/example/ecommerce/model/User.java
+++ src/main/java/com/example/ecommerce/model/User.java
@@ -1,12 +1,16 @@
 package com.example.ecommerce.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Date;
 @Data
 public class User {
     private Integer userId;
+    @JsonProperty("e_mail")
     private String email;
+    @JsonIgnore
     private String password;
     private Date createdDate;
     private Date lastModifiedDate;
